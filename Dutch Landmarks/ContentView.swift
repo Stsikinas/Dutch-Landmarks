0b88bb5450ca3cce3b07ea9b162c7dec181f480c
Stavros Tsikinas <s.tsikinas@epsilonnet.gr>
2021-09-27 12:40:43 +0300

    initial commit

---
+++ Dutch Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dutch Landmarks
+//
+//  Created by Epsilon User on 27/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dutch Landmarks/Dutch_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dutch_LandmarksApp.swift
+//  Dutch Landmarks
+//
+//  Created by Epsilon User on 27/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Dutch_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
